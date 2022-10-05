d629276433b9aeeb0caa316f9463d6357e05b3e9
Asrat Minase <asrat461@revature.net>
2022-10-05 17:12:41 -0500

    This is a backend for Bailey Barista Training Academy

---
+++ src/main/java/com/revature/bailey/courses/Courses.java
@@ -0,0 +1,24 @@
+package com.revature.bailey.courses;
+
+import com.revature.bailey.classes.Classes;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+@Table(name = "courses")
+public class Courses {
+    @Id
+    private String courseid;
+    @Column(unique = true)
+    private String coname;
+    @ManyToOne
+    @JoinColumn(name = "classid", referencedColumnName = "classid")
+    private Classes classid;
+}
---
+++ src/main/java/com/revature/bailey/skills/Skills.java
@@ -0,0 +1,24 @@
+package com.revature.bailey.skills;
+
+import com.revature.bailey.courses.Courses;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+@Table(name = "skills")
+public class Skills {
+    @Id
+    private String skillid;
+    @Column(unique = true)
+    private String sname;
+    @ManyToOne
+    @JoinColumn(name = "courseid", referencedColumnName = "courseid")
+    private Courses courseid;
+}
